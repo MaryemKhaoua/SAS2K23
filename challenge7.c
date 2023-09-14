3abb16d495291d6d048c4f5a791b46bd15c8b146
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-09-14 10:48:52 +0100

    inverse

---
+++ challenge7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter 3 chiffres : \n");
+    printf("Enter a: ");
+    scanf("%d", &a);
+    
+    printf("Enter b: ");
+    scanf("%d", &b);
+    printf("Enter c : ");
+    scanf("%d", &c);
+    printf("l'iverse de nbr est : %d%d%d ", c, b, a);
+    return (0);
+}
